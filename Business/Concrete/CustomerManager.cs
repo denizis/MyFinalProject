8fa5df635f29b4a3813369ca7eaa6cbc0ecb65ae
denizis <deniziskender15@outlook.com>
2024-10-04 15:23:01 +0300

    Proje dosyası ekle.

---
+++ Business/Concrete/CustomerManager.cs
@@ -0,0 +1,55 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+
+        ICustomerDal _customerdal;
+        public CustomerManager(ICustomerDal customerdal)
+        {
+
+
+            _customerdal = customerdal;
+
+        }
+
+        public void Add(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(Expression<Func<Customer, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _customerdal.GetAll();
+        }
+
+        public List<Customer> GetAll(Expression<Func<Customer, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Customer entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            //foreach (var product in productManager.GetAllByCategoryId(2))
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+
+
+
+
+
+            Console.Read();
+        }
+    }
+}
